8b4bf9fec01f4ff73569249990546a8662db1030
Attenzione <atrobz@gmail.com>
2023-03-09 14:25:26 +0100

    Zadanie 4

---
+++ migrations/Version20230309132315.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230309132315 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE history (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, first_in INTEGER NOT NULL, second_in INTEGER NOT NULL, first_out INTEGER NOT NULL, second_out INTEGER NOT NULL, created_at VARCHAR(255) NOT NULL, updated_at VARCHAR(255) NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE history');
+    }
+}
---
+++ src/Entity/History.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HistoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HistoryRepository::class)]
+#[ORM\HasLifecycleCallbacks()]
+class History
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $firstIn = null;
+
+    #[ORM\Column]
+    private ?int $secondIn = null;
+
+    #[ORM\Column]
+    private ?int $firstOut = null;
+
+    #[ORM\Column]
+    private ?int $secondOut = null;
+
+    #[ORM\Column]
+    private ?datetime $createdAt;
+
+    #[ORM\Column]
+    private ?datetime $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstIn(): ?int
+    {
+        return $this->firstIn;
+    }
+
+    public function setFirstIn(int $firstIn): self
+    {
+        $this->firstIn = $firstIn;
+
+        return $this;
+    }
+
+    public function getSecondIn(): ?int
+    {
+        return $this->secondIn;
+    }
+
+    public function setSecondIn(int $secondIn): self
+    {
+        $this->secondIn = $secondIn;
+
+        return $this;
+    }
+
+    public function getFirstOut(): ?int
+    {
+        return $this->firstOut;
+    }
+
+    public function setFirstOut(int $firstOut): self
+    {
+        $this->firstOut = $firstOut;
+
+        return $this;
+    }
+
+    public function getSecondOut(): ?int
+    {
+        return $this->secondOut;
+    }
+
+    public function setSecondOut(int $secondOut): self
+    {
+        $this->secondOut = $secondOut;
+
+        return $this;
+    }
+
+    #[ORM\PrePersist()]
+    public function createdAt()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+    #[ORM\PreUpdate()]
+    public function updateAt()
+    {
+        $this->updatedAt = new \DateTime();
+    }
+}
